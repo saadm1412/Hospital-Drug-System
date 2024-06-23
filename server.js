5d90e6647e62edfe431b26781c7daea974ef4358
Saad Mujawar <saadmujawar1412@gmail.com>
2024-06-23 12:17:24 +0530

    change in server.js

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Create a connection to the MySQL database
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'saadm1412',
-  database: 'apex'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 // Connect to the database
